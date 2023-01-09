a29557496f768b3d813a7cf85087391a3877c7a3
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2023-01-09 01:05:17 -0800

    typo: change if for switch

--- src/assets/Components/CountryApi.jsx
+++ src/assets/Components/CountryApi.jsx
@@ -4,25 +4,7 @@ import axios from 'axios';
 
 const CountryApi = () => {
     const [country, setCountry] = useState({});
-    const [showDemography, setShowDemography] = useState(true);
-    const [showLocation, setShowLocation] = useState(false);
-    const [showCapital, setShowCapital] = useState(false);
-
-    const demographyItem = () => {
-        setShowDemography(!showDemography);
-        setShowLocation(false);
-        setShowCapital(false);
-    }
-    const locationItem = () => {
-        setShowLocation(!showLocation);
-        setShowDemography(false);
-        setShowCapital(false);
-    }
-    const capitalItem = () => {
-        setShowCapital(!showCapital);
-        setShowLocation(false);
-        setShowDemography(false);
-    }
+    const [display, setDisplay] =useState('demography')
 
 
     useEffect(() => {
@@ -31,48 +13,45 @@ const CountryApi = () => {
     },[])
 
 
-    return (
-        <div>
-            <h1>{country.name?.official}</h1>
-            <img src={country.flags?.png} alt={country.name?.official} />
-            <div className="container__btns">
-                <button onClick={demographyItem}>Demography</button>
-                <button onClick={locationItem}>Location</button>
-                <button onClick={capitalItem}>Capital</button>
-            </div>
-            {
-                showDemography ?
-                (
+    const showInfo = () =>{
+        switch(display){
+            case 'demography':
+                return (
                     <>
                         <h3>Demography</h3>
                         <p><b>Population: </b>{country.population}</p>
                         <p><b>Area: </b>{country.area}</p>
                     </>
-                ) : null
-
-            }
-            {
-                showLocation ? (
+                )
+            case 'location':
+                return(
                     <>
                         <h3>Location</h3>
                         <p><b>Continent: </b>{country.continents?.[0]}</p>
                         <p><b>Region: </b>{country.region}</p>
                         <p><b>Subregion: </b>{country.subregion}</p>
                     </>
-                ) : null
-
-            }
-
-            {
-                showCapital ? (
+                )
+            case 'capital':
+                return(
                     <>
                         <h3>Capital</h3>
                         <p><b>Capital: </b>{country.capital}</p>
                     </>
-                ) : null
+                )
+        }
+    }
 
-            }
-
+    return (
+        <div>
+            <h1>{country.name?.official}</h1>
+            <img src={country.flags?.png} alt={country.name?.official} />
+            <div className="container__btns">
+                <button onClick={() => setDisplay('demography')}>Demography</button>
+                <button onClick={() => setDisplay('location')}>Location</button>
+                <button onClick={() => setDisplay('capital')}>Capital</button>
+            </div>
+            {showInfo()}
         </div>
     );
 };
